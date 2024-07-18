418eb7affd5e30166113ea832af7dad90919fb85
Rohit Rana <rohit.rana@advait.org.in>
2024-07-18 10:15:47 +0530

    first commit

---
+++ test1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func main() {
+
+	fmt.Println(math.Sqrt(49))
+	fmt.Println(multiply(20, 40))
+}
